e03d9d677b1adb23a6a31698b778ff3d4109c2bd
Hobbitcubed <hobbitcubed@gmail.com>
2026-01-21 07:37:37 -0600

    Add Interactable Object SO, remove monobehavior

---
+++ Cardomancers/Assets/Scripts/Player/InteractableObject_SO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "InteractableObject_SO", menuName = "Scriptable Objects/InteractableObject_SO")]
+public class InteractableObject_SO : ScriptableObject
+{
+    public new string name;
+    public UnityEvent interacted;
+}
