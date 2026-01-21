8e23b3beca17f6ebe0f76ca07d6a790fc9989209
S1ren <sonictoby08@gmail.com>
2026-01-21 08:05:54 -0600

    moneyyyy

--- Cardomancers/Assets/Scripts/Environment/moneyPickup.cs
+++ Cardomancers/Assets/Scripts/Environment/moneyPickup.cs
@@ -1,11 +1,31 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class moneyPickup : MonoBehaviour
 {
 
     public static int money = 0;
+    public bool respawn = false;
+    public float respawnTime = 15f;
+    public UnityEvent Respawn = new UnityEvent();
+    public UnityEvent OnCollect = new UnityEvent();
 
-    // money on here for testing put on playercontroller later              
+    // money on here for testing put on SO or sum later
+    
+    void Update()
+    {
+        if (respawn)
+        {
+            respawnTime -= Time.deltaTime;
+            if (respawnTime < 0)
+            {
+                Respawn.Invoke();
+                respawn = false;
+                respawnTime = 15;
+            }
+        }
+    }
+    // i dont remember how unity events actually work bruh
 
     void OnCollisionEnter(Collision collision)
     {
@@ -13,6 +33,8 @@ public class moneyPickup : MonoBehaviour
         print (collision);
         money++;
         Debug.Log ($"money: {money}");
+        respawn = true;
+        OnCollect.Invoke();
     }
     // copied from fps game with unneccesary parts removed, will have to update to use interaction script once micah does that
 }
