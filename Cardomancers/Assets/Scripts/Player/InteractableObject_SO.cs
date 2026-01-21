91d06db613c2e3e46da38d4417470dfa9c165b16
STUDENT\JS224691 <js224691@misdmail.net>
2026-01-21 09:04:09 -0600

    Fix Project Local

--- Cardomancers/Assets/Scripts/Player/InteractableObject_SO.cs
+++ Cardomancers/Assets/Scripts/Player/InteractableObject_SO.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 using UnityEngine.Events;
 
 [CreateAssetMenu(fileName = "InteractableObject_SO", menuName = "Scriptable Objects/InteractableObject_SO")]
@@ -6,4 +6,4 @@ public class InteractableObject_SO : MonoBehaviour
 {
     public new string name;
     public UnityEvent<GameObject> interactable;
-}
+}*/
