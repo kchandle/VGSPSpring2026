a8180076c2136e965171249b02c1615722e08029
STUDENT\ad256400 <aaravdesai192010@gmail.com>
2026-01-21 09:45:53 -0600

    updated the player interaction

--- Cardomancers/Assets/PlayerInteract.cs
+++ Cardomancers/Assets/PlayerInteract.cs
@@ -5,26 +5,52 @@ using UnityEngine.InputSystem;
 
 public class PlayerInteract : MonoBehaviour
 {
+    // List<GameObject> hits;
+
+
+
+    [SerializeField] InteractableObject inter;
+    public InputActionReference interact;
+
+  
+
     CapsuleCollider capsule;
     public int range = 5;
 
     void Awake()
     {
         capsule = GetComponent<CapsuleCollider>();
+        inter = GetComponent<InteractableObject>();
+        // hits = new List<GameObject>();
     }
+
+
+
+    public void InteractKey(InputAction.CallbackContext obj)
+    {
+       Debug.Log("Yayy, u inteactedd with mee!");
+    }
+
+
 
     void FixedUpdate()
     {
         RaycastHit hit;
 
         //Radius in which objects can be interacted with
-        if (Physics.SphereCast(transform.position, capsule.radius * range, transform.forward, out hit))
+        Collider[] col = Physics.OverlapSphere(transform.position, range);
         {
             //If object is interactable, do stuff
-            if(hit.transform.gameObject.TryGetComponent<IInteractable>(out IInteractable interactable))
+            print(col);
+
+
+
+
+
+            /*if(hit.transform.gameObject.TryGetComponent<>(out  interactable))
             {
-                interactable.Interacted();
-            }
+                
+            }*/
         }
 
     }
