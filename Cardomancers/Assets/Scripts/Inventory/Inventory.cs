f388e00a55834f1213fdbf594b11c161cc29a4db
N-cataria <175165771+N-cataria@users.noreply.github.com>
2026-01-23 09:04:03 -0600

    inventory

--- Cardomancers/Assets/Scripts/Inventory/Inventory.cs
+++ Cardomancers/Assets/Scripts/Inventory/Inventory.cs
@@ -12,19 +12,37 @@ public class Inventory : MonoBehaviour
 	[SerializeField] private GameObject inventoryCardPrefab;
 
 	// all the cards the player has
-	[SerializeField] private InventoryCard[] inventory;
+	[SerializeField] private List<InventoryCard> inventory;
 	// cards that can be used when in battle
-	[SerializeField] private InventoryCard[] deck;
+	[SerializeField] private List<InventoryCard> deck;
 	// total size of inventory
 	[SerializeField] private int inventorySize;
 	// amount of cards the player is alowed to have in their deck at one time
 	[SerializeField] private int deckSize;
 
-	//returns true if card is successfully added to inventory otherwise returns false
+	// all add card to x methods return true if card is successfully added to inventory otherwise returns false
+
 	public bool AddCardToInventory(Card card)
 	{
-		// stops the method and retuurns false
-		if(inventory.length + 1 > deckSize) return false;
-		InventoryCard[] temp = new InventoryCard[inventory.length + 1];
+		// stops the method and returns false if the inventory is full
+		if(inventory.Count >= deckSize) return false;
+		// very temporary
+		newInventoryCard = new InventoryCard(card);
+		// add new card to deck
+		inventory.Add(newInventoryCard);
+		// automatically add to deck if possible
+		if(deck.Count >= deckSize) AddCardToDeck(newInventoryCard);
+		return true;
 	}
+
+	public bool AddCardToDeck(InventoryCard card)
+	{
+		//don't add a card to the deck if the deck is full
+		if(deck.Count >= deckSize) return false;
+		// stop if the inventory doesn't contain the card
+		if(!inventory.Contains(card)) return false;
+		deck.Add(card);
+		return true;
+	}
+
 }
