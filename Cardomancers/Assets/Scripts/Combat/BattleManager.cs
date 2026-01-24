270880a90fa177ba4b80da1ee74777100171262c
Feild-Null <jurrasicwreck@gmail.com>
2026-01-24 11:45:03 -0600

    Updated BattleManager

--- Cardomancers/Assets/Scripts/Combat/BattleManager.cs
+++ Cardomancers/Assets/Scripts/Combat/BattleManager.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using static BattleManager;
 
 public class BattleManager : MonoBehaviour
 {
@@ -19,14 +22,28 @@ public class BattleManager : MonoBehaviour
     public UnityEvent EnemyTurn; // event triggered at the start of the enemy's turn
     #endregion
 
-    //public Battle_SO battle; // current battle SO passed in when battlestart is called
+    #region UI Elements
+    public Canvas winScreen; // the canvas displayed when the player wins
+    public Canvas loseScreen; // the canvas displayed when the player loses
+    #endregion
 
+    public Battle_SO battle; // current battle SO passed in when battlestart is called
+    public BattleState battleState; // current state of the battle
 
+    public InputActionAsset inputActions; // reference to the input system
+
+    public enum BattleState
+    {
+        START,
+        PLAYER_TURN,
+        ENEMY_TURN,
+        WON,
+        LOST
+    }
+
+    #region Setup
     private void Awake()
     {
-        battleCamera.GetComponent<Camera>();
-        mainCamera.GetComponent<Camera>();
-
         // Check if an instance already exists
         if (instance != null && instance != this)
         {
@@ -40,6 +57,9 @@ public class BattleManager : MonoBehaviour
 
         // Optional: Keep the object alive when loading new scenes
         DontDestroyOnLoad(this.gameObject);
+
+        battleCamera.GetComponent<Camera>();
+        mainCamera.GetComponent<Camera>();
     }
 
     private void OnEnable()
@@ -50,22 +70,133 @@ public class BattleManager : MonoBehaviour
         PlayerTurn.AddListener(() => Debug.Log("Player's Turn"));
         EnemyTurn.AddListener(() => Debug.Log("Enemy's Turn"));
     }
+    #endregion
 
-
-    //public void StartBattle(Battle_SO battle)
-    //{
-
-    //}
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //Function called by an outside force to start a battle, must pass in battle_SO
+    public void StartBattle(Battle_SO battle)
     {
-        
+        // Spawn enemies based on the Battle_SO
+        this.battle = battle;
+
+        //Switches Camera to Battle camera
+        mainCamera.enabled = false;
+        battleCamera.enabled = true;
+
+        //Shuffle and arrange both player and enemy decks
+
+
+        StartCoroutine(TurnManager());
+        battleState = BattleState.PLAYER_TURN;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator TurnManager()
     {
+        while (battleState != BattleState.WON || battleState != BattleState.LOST) {
+            StartPlayerTurn();
+            //Wait for player to finish turn
+            while (battleState == BattleState.PLAYER_TURN)
+            {
+                yield return null;
+            }
+            StartEnemyTurn();
+            //Wait for enemy to finish turn
+            while (battleState == BattleState.ENEMY_TURN)
+            {
+                yield return null;
+            }
+        }
+        EndBattle();
+        yield return null;
+    }
+
+
+    public void StartPlayerTurn()
+    {
+        OnBattleStart.Invoke();
+        PlayerTurn.Invoke();
+        //Check if player is out of cards
+
+        //Display cards
+
+        // // Enables player drage and drop input if not already enabled
+        //if (!inputActions.FindActionMap("DragAndDrop").enabled)
+        //{
+        //    inputActions.FindActionMap("DragAndDrop").Enable();
+        //}
+
+        // Start Player turn coroutine to handle playing cards
+
+        //On card raises played event: Start iterating through status effects and apply, leaving space to add an animation as the status's iterate
+
+        // If player or enemy is out of health, change battleState to WON or LOST
+
+        //After player turn: battleState = BattleState.ENEMY_TURN;
+    }
+
+    public void StartEnemyTurn()
+    {
+        // // Disables player drag and drop input if not already disabled
+        //if (inputActions.FindActionMap("DragAndDrop").enabled)
+        //{
+        //    inputActions.FindActionMap("DragAndDrop").Disable();
+        //}
+
+        //Check if enemy is out of cards
+
+        //Display cards
+
+        EnemyTurn.Invoke();
+
+        //Enemy picks card from card list
+
+        // If player or enemy is out of health, change battleState to WON or LOST
+
+        //On card raises played event: battleState = BattleState.PLAYER_TURN;
+
+    }
+
+    public void EndBattle()
+    {
+        // Depending on battleState, invoke win or lose events
+        switch (battleState)
+        {
+            case BattleState.WON:
+                OnWin.Invoke();
+                // Display win screen
+                winScreen.enabled = true;
+
+                //Start win coroutine
+                //Get rewards from SO and display
+
+                break;
+            case BattleState.LOST:
+                OnLose.Invoke();
+                // Display lose screen
+                loseScreen.enabled = true;
+
+                //start loss coroutine
+                //If player lost, return to last checkpoint or main menu
+
+                break;
+        }
+
+        //Stops the turn manager coroutine
+        StopCoroutine(TurnManager());
         
+        //Switches Camera back to Main camera
+        battleCamera.enabled = false;
+        mainCamera.enabled = true;
+
+        //Hides win/lose screen after battle ends
+        if (loseScreen.enabled == true)
+        {
+            loseScreen.enabled = false;
+        } else if (winScreen.enabled == true)
+        {
+            winScreen.enabled = false;
+        }
+
+
     }
 }
