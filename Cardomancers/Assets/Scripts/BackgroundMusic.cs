e82207d02abb35046a67bc650fb3adc2ebe9fa80
DerjenigeUberMensch <119084558+DerjenigeUberMensch@users.noreply.github.com>
2026-02-02 08:31:42 -0600

    Added BackgroundMusic Script

---
+++ Cardomancers/Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,53 @@
+// Debugging 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(AudioSource))]
+public class BackgroundMusic : MonoBehaviour
+{
+    [Tooltip("The Background Music Audio Clip To Play.")]
+    [SerializeField] public AudioClip audioClip = null;
+    [Tooltip("How Loud You Want The Background Music Audio Clip Be.")]
+    [Range(0f, 1f)]
+    [SerializeField] public float Volume = 1.0f;
+
+    // Allow other scripts to interface with this if they need too.
+    [NonSerialized] public AudioSource audioSource;
+
+    void Start()
+    {
+        // No audio clip to play so throw error msg.
+        if(!this.audioClip)
+        {   Debug.LogError("No audio clip found in Background Music Script.");
+        }
+        else
+        {
+            // This sets the settings for the AudioSource.
+
+            this.audioSource = GetComponent<AudioSource>();
+            this.audioSource.volume = Volume;
+            this.audioSource.clip = this.audioClip;
+            this.audioSource.loop = true;
+        }
+
+        this.audioSource.Play();
+    }
+
+    void Update()
+    {
+        // If during testing we want to make it sound different this handles that.
+        #if UNITY_EDITOR
+            this.audioSource.volume = Volume;
+        #endif
+    }
+
+    void OnDestroy()
+    {   
+        // stop playing on destroy
+        this.audioSource.Stop();
+    }
+}
