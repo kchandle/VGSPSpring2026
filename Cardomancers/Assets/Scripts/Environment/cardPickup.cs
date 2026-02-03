410af4532349bdf5fed09b6e7a1a2ae54ce243c8
DESKTOP-3NA92R1\sonic <sonictoby08@gmail.com>
2026-02-03 14:49:12 -0600

    added card pickup script

---
+++ Cardomancers/Assets/Scripts/Environment/cardPickup.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class cardPickup : MonoBehaviour
+{
+    // public InventoryCard card;
+    void getCard()
+    {
+      //  Inventory.Inventory.add(card);
+    }
+
+    // Puts card in inventroy :)
+}
