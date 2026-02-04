b4f4bf1a1589965416597eff9c234c5b9f5f8979
N-cataria <175165771+N-cataria@users.noreply.github.com>
2026-02-04 08:26:59 -0600

    inventory hacking testing

--- Cardomancers/Assets/Scripts/Inventory/InventoryHack.cs
+++ Cardomancers/Assets/Scripts/Inventory/InventoryHack.cs
@@ -1,22 +1,39 @@
 #nullable enable
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
 public class InventoryHack : PlayItem
 {
+    // the hack that will be added
     [SerializeField] private Hack_SO hack;
+    // the card that the hack will be added to
     private Card cardToAddHack;
-    private Playspace playspace;
+    // List of all the playspaces in the scene
+    private List<Playspace> playspaces;
 
+    // populate playspace list
     private void Awake()
     {
-        hack = GetComponentInParent<Hack_SO>();
+        playspaces = FindObjectsByType<Playspace>(FindObjectsSortMode.None).ToList();
     }
 
     public void AddHackToCard()
     {
+        if (!hack)
+        {
+            Debug.LogWarning("No Hack_SO attached");
+            return;
+        }
+        if (!cardToAddHack)
+        {
+            Debug.LogWarning("No card detected");
+            return;
+        }
         if(cardToAddHack.hacks.Count < cardToAddHack.maxHacks)
         {
             cardToAddHack.hacks.Add(hack);
+            Debug.Log(cardToAddHack.hacks);
         }
         else
         {
@@ -26,13 +43,17 @@ public class InventoryHack : PlayItem
 
     private Card? FindCard()
     {
-        foreach (PlayItem item in playspace.playItems)
+        foreach (Playspace playspace in playspaces)
         {
-            if (item.BoxCollider.IsTouching(this.BoxCollider) && item is Card)
+            foreach (PlayItem item in playspace.playItems)
             {
-                return item as Card;
+                if (item.BoxCollider.IsTouching(this.BoxCollider) && item is Card)
+                {
+                    return item as Card;
+                }
             }
         }
+
         return null;
     }
 
