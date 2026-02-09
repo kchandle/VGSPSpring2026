5bca137b19801d335430c89add5e3cee1efe335b
overcastedSays <overcastsays@gmail.com>
2026-02-09 14:28:17 -0600

    Starting to fix inv gui input bugs

--- Cardomancers/Assets/Player Input/PlayerController.cs
+++ Cardomancers/Assets/Player Input/PlayerController.cs
@@ -75,14 +75,17 @@ public class PlayerController : MonoBehaviour
 
 	public void OnJumping(InputAction.CallbackContext context)
 	{
+       
         //returns if it isnt the frame that it is pressed
         if (!context.started) return;
+         print("HELLO JUMPING");
 		// makes the player jump
 	    _characterControllerMovement.jumping = true; 
 	}
 
    public void OnToggleInventory(InputAction.CallbackContext context)
     {
+        print("TABBED");
         //can only open the inventory when in free movement and alive
         if (GameStateScript.CurrentState == GameStateScript.GameState.WALKING && inventoryUIHandler.uiDisplayed == false)
         {
