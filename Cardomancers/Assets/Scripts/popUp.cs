770cfa63ff0527bb09ecbb1ce20c0c39506e3d44
S1ren <sonictoby08@gmail.com>
2026-02-12 09:54:08 -0600

    Made pop up scripts and SO

--- Cardomancers/Assets/Scripts/popUp.cs
+++ Cardomancers/Assets/Scripts/popUp.cs
@@ -5,23 +5,38 @@ using System.Collections;
 
 public class popUp : MonoBehaviour
 {
-    // public popUp_SO;
+    public popUp_SO popSO;
     public Image image;
     public TextMeshProUGUI popUpText;
     public GameObject canvas;
     private float timer;
-    
-    public void onEnabled()
+
+    public void SetVariables(popUp_SO popSO)
     {
-        popUpText.text = popUp_SO.Text;
+        this.popSO = popSO;
+        timer = popSO.timer;
+        SettingActive();
+    } 
+    // Script called by other scripts that incur popups, passing in their popup_SO
+
+
+    public void SettingActive()
+    {
         canvas.SetActive(true);
+        popUpText.text = popSO.popUpText;
         StartCoroutine(popUpTimer(timer));
     }
+    // Sets canvas active and starts coroutine
 
     IEnumerator popUpTimer(float Timer)
     {
+        print(Timer);
+        Timer = timer;
         yield return new WaitForSeconds(Timer); 
+        // Counts down time until popup ends
 
         canvas.SetActive(false);
+        popUpText.text = string.Empty;
+        // Sets canvas inactive and empties textbox for next use
     }
 }
