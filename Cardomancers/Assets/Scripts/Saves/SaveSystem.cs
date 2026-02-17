a4c9fb30b5d37229bd25e930a01e470b0450daca
n-cataria <shafferjesse25@gmail.com>
2026-02-17 16:11:40 -0600

    String to Byte[]
    Switched from writing the file based on the string provided by the JsonUtilities class to a byte array, so it can be more easily encrypted in the future.

--- Cardomancers/Assets/Scripts/Saves/SaveSystem.cs
+++ Cardomancers/Assets/Scripts/Saves/SaveSystem.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using System.IO;
+using System.Text;
+using System.Security.Cryptography;
 
 public static class SaveSystem
 {
     // The file path data is saved to
-    private static string dataPath =>  Path.Combine(Application.persistentDataPath, "save-data.json");
+    private static string DataPath =>  Path.Combine(Application.persistentDataPath, "save-data.json");
+    // key used to encrypt save data
+    private static byte[] key = new byte[32];
 
     // Takes in an inventory SO and the game object for the player and turns it into a JSON file
     public static void Save(Inventory_SO inventory, GameObject player)
@@ -20,18 +24,29 @@ public static class SaveSystem
             rotation = player.transform.eulerAngles
         };
 
+        // Serialize save data into JSON
+        string json = JsonUtility.ToJson(data);
+        //Convert to bytes so that the save data can be encrypted
+        byte[] jsonBytes = Encoding.UTF8.GetBytes(json);
+        //using (Aes aes = Aes.Create())
+        //{
+            //aes.Key = key;
+            //aes.IV = aes.GenerateIV();
+        //}
+        
+
         // Creates or overwrites save file with readable file structure
-        File.WriteAllText(dataPath, JsonUtility.ToJson(data, true));
+        File.WriteAllBytes(DataPath, jsonBytes);
     }
 
     // Takes in an inventory SO and assigns its data based on the saved data
     public static void Load(Inventory_SO inventory, GameObject player)
     {
         // Ends function if there is no save data
-        if (!File.Exists(dataPath)) return;
+        if (!File.Exists(DataPath)) return;
        
         // where data is assigned
-        SaveData data = JsonUtility.FromJson<SaveData>(File.ReadAllText(dataPath));
+        SaveData data = JsonUtility.FromJson<SaveData>(File.ReadAllText(DataPath));
         inventory.Inventory = data.inventory;
         inventory.Deck =  data.deck;
         inventory.DeckLength = data.deckLength;
