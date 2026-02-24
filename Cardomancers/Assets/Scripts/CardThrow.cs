22d2e9b800599f8ee130dc9862b4548e3bcab062
GingerBird777 <jacksonboyer0327@gmail.com>
2026-02-24 08:23:37 -0600

    wicked

--- Cardomancers/Assets/Scripts/CardThrow.cs
+++ Cardomancers/Assets/Scripts/CardThrow.cs
@@ -49,6 +49,7 @@ public class CardThrow : MonoBehaviour
 
             //resets the card instance position, sets it to actives and gets the rigidbody
             card.transform.position = transform.position;
+            card.transform.rotation = Quaternion.Euler(Vector3.zero);
             card.SetActive(true);
             Rigidbody cardRB = card.GetComponent<Rigidbody>();
 
