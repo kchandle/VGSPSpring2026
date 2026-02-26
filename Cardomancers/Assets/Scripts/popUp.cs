0bfca13a511ed8f16cd9397d6282de20af8074ce
S1ren <sonictoby08@gmail.com>
2026-02-26 09:00:59 -0600

    Updates to Pop Up, including bugfixes and yes/no decisions added

--- Cardomancers/Assets/Scripts/popUp.cs
+++ Cardomancers/Assets/Scripts/popUp.cs
@@ -6,31 +6,51 @@ using System.Collections;
 public class popUp : MonoBehaviour
 {
     public popUp_SO popSO;
-    public Image image;
+    //public Image image;
     public TextMeshProUGUI popUpText;
+    // "Canvas" is the TEXT component that gets turned active. Was previously the actual canvas but i made it better and im too lazy to rewrite
     public GameObject canvas;
-    public GameObject yesorno;
+    public GameObject yes;
+    public GameObject no;
     // "Decision" if the pop up has a yes or no
-    public bool decision;
+    private bool decision = false;
+    // choice depends on which button hit
+    public bool choice;
     private float timer;
+    private bool responseMade = false;
 
     public void SetVariables(popUp_SO popSO)
     {
         this.popSO = popSO;
+        popUpText.text = popSO.popUpText;
         timer = popSO.timer;
         decision = popSO.decision;
+        responseMade = false;
         SettingActive();
     } 
     // Script called by other scripts that incur popups, passing in their popup_SO
 
+    public void OnYesClick()
+    {
+        choice = true;
+        responseMade = true;
+    }
+
+    public void OnNoClick()
+    {
+        choice = false;
+        responseMade = true;
+    }
+
+    // Set by yes/no buttons, Decision can be used by script that calls and responseMade lets the coroutine pass through
 
     public void SettingActive()
     {
         canvas.SetActive(true);
-        popUpText.text = popSO.popUpText;
         if (decision) 
         {
-            yesorno.SetActive(true);
+            yes.SetActive(true);
+            no.SetActive(true);
         }
         StartCoroutine(popUpTimer(timer));
     }
@@ -38,13 +58,24 @@ public class popUp : MonoBehaviour
 
     IEnumerator popUpTimer(float Timer)
     {
-        print(Timer);
-        Timer = timer;
-        yield return new WaitForSeconds(Timer); 
-        // Counts down time until popup ends
+        if (!decision)
+        {
+            print(Timer);
+            Timer = timer;
+            yield return new WaitForSeconds(timer); 
+            // Counts down time until popup ends. Not ran if decision is active
+        }
+
+        if(decision)
+        {
+            yield return new WaitUntil(() => responseMade);
+        }
 
         canvas.SetActive(false);
         popUpText.text = string.Empty;
+        yes.SetActive(false);
+        no.SetActive(false);
+
         // Sets canvas inactive and empties textbox for next use
     }
 }
