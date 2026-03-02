7e4a6f8888b4696deb0a5599a1e62d8390ab1d24
N-cataria <175165771+N-cataria@users.noreply.github.com>
2026-03-02 09:05:33 -0600

    Add cards to inventory if inventory is empty on load

--- Cardomancers/Assets/Scripts/Saves/LoadGame.cs
+++ Cardomancers/Assets/Scripts/Saves/LoadGame.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class LoadGame : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+    [SerializeField] private Card_SO testDefaultCard;
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         Inventory.CardsDatabase = Resources.LoadAll<Card_SO>("Card_SO").ToDictionary(k => k.name, v => v);
         Inventory.HacksDatabase = Resources.LoadAll<Hack_SO>("Hack_SO").ToDictionary(k => k.name, v => v);
+        if(Inventory.InventoryList.Count == 0) Inventory.AddCardToInventory(testDefaultCard, 3);
         SaveSystem.Load(player);
         Destroy(this);
     }
