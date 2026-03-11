953c1d27b3883fcc18d2060fd480c54b7fecca7f
Hobbitcubed <hobbitcubed@gmail.com>
2026-03-11 14:49:01 -0500

    Enemy Attack Movement, Tested emojis, Fixed stuff in prealpha wand district test, added youngsters pngs

--- Cardomancers/Assets/Scripts/Player/Player Input/PlayerController.cs
+++ Cardomancers/Assets/Scripts/Player/Player Input/PlayerController.cs
@@ -121,6 +121,7 @@ public class PlayerController : MonoBehaviour
             inventoryUIHandler.DestroyUI();
             GameStateScript.CurrentState = GameStateScript.GameState.WALKING;
         }
+        
     }
     public IEnumerator StatusEffects()
     {
