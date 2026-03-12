b752132bfc08faa6a4c691dbdc5e1f4b51b70afd
STUDENT\JS224691 <js224691@misdmail.net>
2026-03-12 07:10:54 -0500

    Finished Shop UI Code

---
+++ Cardomancers/Assets/Scripts/Shop UI/ShopSlot.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopSlot : MonoBehaviour
+{
+    [SerializeField] public  ShopItem shopItem; //NOT set in editor
+    [SerializeField] private ShopPanel shopPanel; //set in editor
+    [SerializeField] private Image image; //set in editor
+
+    public void Init(ShopItem item)
+    {
+        shopItem = item;
+        image.sprite = shopItem.SO.image;
+    }
+
+    public void Init(Card_SO so)
+    {
+        shopItem.Init(so);
+        image.sprite = shopItem.SO.image;
+    }
+
+    //called when the object is clicked on  
+    public void UpdateShopPanel()
+    {
+        shopPanel.UpdatePanel(shopItem);
+    }
+}
---
+++ Cardomancers/Assets/Scripts/Shop UI/ShopUI.cs
@@ -0,0 +1,397 @@
+/* Author: DerjenigeUberMensch
+ *
+ * Contact Group 1 For help or questions relating to this script.
+ */
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Networking;
+using UnityEngine.EventSystems;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine.UI;
+
+public class ShopUI : MonoBehaviour
+{
+    // ui elements
+    private GameObject canvas;
+    private GameObject scrollview;
+    private GameObject viewport;
+    private GameObject cardTemplate;
+    [SerializeField] private GameObject content; //set in editor
+    [SerializeField] private GameObject sellContent; //set in editor
+    [SerializeField] private TMPro.TextMeshProUGUI playerMoneyText; //set in editor
+    [SerializeField] private GameObject shopSlotTemplate; //set in editor, is the lone cardShopTemplate object under IgnoreContent. This was more convenient than making a prefab.
+    private MusicPlayer musicPlayer;
+
+    [SerializeField] private Shop shop = new();
+    //[SerializeField] private List<Card_SO> stock; //set in editor
+    [SerializeField] private List<(GameObject obj, ShopItem shopItem)> cards = new();
+
+    [Tooltip("Volume when not inside the Shop UI")]
+    public float UnfocusVolume = .65f;
+    [Tooltip("Volume when inside the Shop UI")]
+    public float FocusVolume = .9f;
+
+    public static GameObject GetUIElementUnderMouse()
+    {
+        PointerEventData pointerData = new PointerEventData(EventSystem.current)
+        {
+            position = Input.mousePosition
+        };
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerData, results);
+
+        return results.Count > 0 ? results[0].gameObject : null;
+    }
+
+    //private void 
+
+    void Start()
+    {
+        Transform root = gameObject.transform;
+        /*this.cardTemplate = root.Find("CardShopTemplate")?.gameObject;
+        this.scrollview = root.Find("Scroll View")?.gameObject;
+        this.viewport = root.Find("Scroll View/Viewport")?.gameObject;*/
+
+        this.canvas = gameObject;
+
+        this.musicPlayer = gameObject.GetComponent<MusicPlayer>();
+
+        this.musicPlayer.Play();
+
+        shop.StockUpdate += OnStockUpdate;
+
+        shop.StockSize = 10;
+
+        CreateBuyMenu();
+        CreateSellMenu();
+    }
+
+    void OnStockUpdate()
+    {
+        // mark all cards as dirty unless we find them.
+        List<int> dirty = Enumerable.Range(0, this.cards.Count).ToList();
+
+        // try and find cards.
+        foreach(ShopItem item in shop.stock)
+        {
+            int index = this.cards.FindIndex(tuple => tuple.shopItem == item);
+            bool found = index != -1;
+
+            if(found)
+            {   dirty.Remove(index);
+            }
+            else
+            {   
+                GameObject clone = Instantiate(this.cardTemplate, this.content.transform.parent);
+
+                this.cards.Add((clone, item));
+            }
+        }
+
+        // sort to decending list.
+        dirty.Sort((a, b) => b.CompareTo(a));
+
+        foreach (int index in dirty)
+        {
+            if (index >= 0 && index < this.cards.Count)
+            {   
+                (GameObject obj, ShopItem unused) = this.cards[index];
+
+                Destroy(obj);
+
+                this.cards.RemoveAt(index);
+            }
+        }
+    }
+
+    void Update()
+    {
+        GameObject element = ShopUI.GetUIElementUnderMouse();
+
+        if(element == null || !element.activeInHierarchy)
+        {   
+            this.musicPlayer.Volume = this.UnfocusVolume;
+            return;
+        }
+
+        if(element.transform.IsChildOf(this.canvas.transform))
+        {   
+            this.musicPlayer.Volume = this.FocusVolume;
+        }
+
+        if(!element.transform.IsChildOf(this.content.transform))
+        {   
+            return;
+        }
+
+        // Element is part of content so that means its in the shop ui.
+    }
+
+
+    //=====================Buying menu=====================//
+    //Create list of shopSlotTemplates, This method should only be called at start
+    public void CreateBuyMenu(List<ShopItem> exclude = null)
+    {
+        print("CREATING BUY MENU");
+        playerMoneyText.text = "$" + Inventory.Money;
+
+        //Destroy any possible existing shop slots
+        while (content.transform.childCount > 0)
+        {
+            DestroyImmediate(content.transform.GetChild(0).gameObject);
+        }
+
+        int i = 1;
+        foreach(ShopItem item in shop.stock)
+        {
+            // exclude any in the exclude list.
+            if(exclude != null && exclude.Any(exc => exc.SO == item.SO))
+            {   
+                continue;
+            }
+
+            GameObject cardSlot = Instantiate(shopSlotTemplate, new Vector3(0,0,0), Quaternion.identity);
+            cardSlot.transform.SetParent(content.transform, false);
+            //cardSlot.GetComponent<ShopItem>().Init(item.SO);
+            cardSlot.GetComponent<ShopSlot>().Init(item);
+            cardSlot.SetActive(true);
+            cardSlot.gameObject.name = "Buy " + (i);
+            i++;
+        }
+
+        UpdateBuyMenu();
+    }
+
+    //Gets cards from the shop's stock and creates slots using the cardShopTemplate prefab to display them 
+    public void UpdateBuyMenu(List<ShopItem> exclude = null)
+    { 
+        print("UPDATING BUY MENU");
+        playerMoneyText.text = "$" + Inventory.Money;
+        
+        //Set all shop slots inactive to begin with
+        //Regular for loops for indexing
+        int i = 0;
+        for(i = 0; i < content.transform.childCount; i++)
+        {
+            content.transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        //Update needed shop slots, leave any remaining ones inactive
+        for(i = 0; i < shop.stock.Count; i++)
+        {
+            ShopItem item = shop.stock[i];
+            content.transform.GetChild(i).gameObject.GetComponent<ShopSlot>().Init(item);
+            content.transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+    
+
+    //Called by the test button OnClick event. 
+    public void GiveMoney() 
+    {
+        Inventory.Money += 1000;
+        playerMoneyText.text = "$" + Inventory.Money;
+        int i = 0;
+        print("Cards: ");
+        foreach(InventoryCard card in Inventory.InventoryList)
+        {
+            i++;
+            print($"Card {i}: {card.cardSO.displayName}");
+        }
+    }
+    
+
+    //=====================Selling menu=====================//
+    //Gets cards from the Inventory and creates slots using the cardShopTemplate prefab to display them
+    public void CreateSellMenu(List<ShopItem> exclude = null)
+    { 
+        print("CREATING SELL MENU");
+        playerMoneyText.text = "$" + Inventory.Money;
+
+        //Destroy any possible existing shop slots
+        while (sellContent.transform.childCount > 0)
+        {
+            DestroyImmediate(sellContent.transform.GetChild(0).gameObject);
+        }
+
+        //Create enough shop slots in the sell menu for all cards in the player's inventory
+        int i = 0;
+        for(i = 0; i < Inventory.InventorySize; i++)
+        {
+            GameObject cardSlot = Instantiate(shopSlotTemplate, new Vector3(0,0,0), Quaternion.identity);
+            cardSlot.transform.SetParent(sellContent.transform, false);
+            cardSlot.gameObject.name = "Sell " + (i+1);
+        }
+
+        UpdateSellMenu();
+    }
+
+    //Activates and deactivates shopSlotTemplates as needed according to the cards in the player's inventory
+    public void UpdateSellMenu(List<ShopItem> exclude = null)
+    {
+        print("UPDATING SELL MENU");
+        playerMoneyText.text = "$" + Inventory.Money;
+
+        //Set all shop slots inactive to begin with
+        int i = 0;
+        for(i = 0; i < sellContent.transform.childCount; i++)
+        {
+            sellContent.transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        //Set shop slots that contain player Inventory cards active, leave others inactive
+        for(i = 0; i < Inventory.InventoryList.Count; i++)
+        {
+            InventoryCard invCard = Inventory.InventoryList[i];
+
+            // exclude any in the exclude list.
+            if(exclude != null && exclude.Any(exc => exc.SO == invCard.cardSO))
+            {   
+                continue;
+            }
+
+            //Every card made has a default hack length of 2
+            if(invCard.hacks.Length <= 2)
+            {
+                ShopItem item = new ShopItem();
+                item.Init(invCard.cardSO);
+
+                sellContent.transform.GetChild(i).gameObject.GetComponent<ShopSlot>().Init(item);
+                sellContent.transform.GetChild(i).gameObject.SetActive(true);
+            }
+        }
+    }
+
+    
+
+
+
+
+
+
+    //Methods were for testing, no longer in use
+    /*//Adds cards to stock. Currently unused.
+    public void AddCardSOToStock(Card_SO card, int amount = 1)
+    {
+        for(int i = 0; i < amount; i++)
+        {
+            stock.Add(card);
+        }
+    }
+
+    //Removes card from stock. Currently used when a card is bought.
+    public void RemoveCardSOFromStock(Card_SO card)
+    {
+        stock.Remove(card);
+    }
+
+    //v1
+    public void UpdateBuyMenu(List<ShopItem> exclude = null)
+    { 
+        //Destroy old items to make way for new ones
+        while (content.transform.childCount > 0)
+        {
+            DestroyImmediate(content.transform.GetChild(0).gameObject);
+        }
+
+         
+        print("Generating stock");
+        foreach(Card_SO card in stock)
+        {
+            print("stock: " + card.displayName);
+            // exclude any in the exclude list.
+            if(exclude != null && exclude.Any(exc => exc.SO == card))
+            {   
+                continue;
+            }
+
+            GameObject cardSlot = Instantiate(shopSlotTemplate, new Vector3(0,0,0), Quaternion.identity);
+            cardSlot.transform.SetParent(content.transform, false);
+            cardSlot.GetComponent<ShopItem>().Init(card);
+            cardSlot.SetActive(true);
+        }
+        print("Finished generating stock");
+    }*/
+
+
+    //v2
+    /*public void UpdateBuyMenu(List<ShopItem> exclude = null)
+    { 
+        //Destroy old items to make way for new ones
+        while (content.transform.childCount > 0)
+        {
+            DestroyImmediate(content.transform.GetChild(0).gameObject);
+        }
+
+         
+        //print("Generating stock");
+        foreach(ShopItem item in shop.stock)
+        {
+            //print("stock: " + item.SO.displayName);
+            // exclude any in the exclude list.
+            if(exclude != null && exclude.Any(exc => exc.SO == item.SO))
+            {   
+                continue;
+            }
+
+            GameObject cardSlot = Instantiate(shopSlotTemplate, new Vector3(0,0,0), Quaternion.identity);
+            cardSlot.transform.SetParent(content.transform, false);
+            //cardSlot.GetComponent<ShopItem>().Init(item.SO);
+            cardSlot.GetComponent<ShopSlot>().Init(item);
+            cardSlot.SetActive(true);
+        }
+        //print("Finished generating stock");
+    }*/
+
+
+    //v1
+    /*public void UpdateSellMenu(List<ShopItem> exclude = null)
+    { 
+        playerMoneyText.text = "$" + Inventory.Money;
+
+        //Destroy all old items in the sell menu in order to create the new ones without issue
+        while (sellContent.transform.childCount > 0)
+        {
+            //print("Destroying the sell children");
+            DestroyImmediate(sellContent.transform.GetChild(0).gameObject);
+        }
+
+
+        //print("creating sell menu");
+        foreach(InventoryCard invCard in Inventory.InventoryList)
+        {
+            // exclude any in the exclude list.
+            if(exclude != null && exclude.Any(exc => exc.SO == invCard.cardSO))
+            {   
+                continue;
+            }
+
+            //print("Card: " + invCard.cardSO.displayName + ". Hack length: " + invCard.hacks.Length);
+
+
+            //Every card made has a hack length of 2
+            if(invCard.hacks.Length <= 2)
+            {
+                //print("creating " + invCard.cardSO.displayName);
+                
+                GameObject cardSlot = Instantiate(shopSlotTemplate, new Vector3(0,0,0), Quaternion.identity);
+                cardSlot.transform.SetParent(sellContent.transform, false);
+
+                ShopItem item = new ShopItem();
+                item.Init(invCard.cardSO);
+
+                cardSlot.GetComponent<ShopSlot>().Init(item);
+                cardSlot.SetActive(true);
+            }
+
+        }
+        //print("finished creating sell menu");
+
+        //print("test: " + Inventory.InventoryList[1].cardSO.displayName);
+    }*/
+}
