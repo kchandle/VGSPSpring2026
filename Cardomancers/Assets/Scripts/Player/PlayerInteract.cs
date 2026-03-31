e5d7c1d119c7ff6339fcb2e5aed606b876b969c9
GingerBird777 <jacksonboyer0327@gmail.com>
2026-03-31 08:19:17 -0500

    temp edit player interact

--- Cardomancers/Assets/Scripts/Player/PlayerInteract.cs
+++ Cardomancers/Assets/Scripts/Player/PlayerInteract.cs
@@ -1,15 +1,18 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.XR.Haptics;
 
 public class PlayerInteract : MonoBehaviour
 {
     public bool interacting = false;
     // the range of the area player can interact with things in:
      public int range = 5;
+
+    private void Update()
+    {
+        
+    }
+
 
     //if the interactkey is set to being interacted or whatever, basically if u press the key:
     public void OnInteract(InputAction.CallbackContext obj)
@@ -24,19 +27,26 @@ public class PlayerInteract : MonoBehaviour
         // sends an array thing to get all objects:
         Collider[] col = Physics.OverlapSphere(transform.position, range);
         {
+            float minRange = 1000f;
             //If object is interactable, so basically if it has the interactable object script, do what it needs to do:
             foreach (Collider c in col)
             {
+                InteractableObject interactable;
                 if (c.TryGetComponent(out InteractableObject inter))
                 {
-                    print(inter);
-                    interacting = true;
-                    inter.interactable.Invoke();
+                    float range = (inter.transform.position - transform.position).magnitude;
+                    if (range < minRange)
+                    {
+                        interactable = inter;
+                        minRange = range;
+                    }
                 }
+                interacting = true;
+                inter.interactable.Invoke();
             }  
         }
-        
- 
     }
+
+
 
 }
