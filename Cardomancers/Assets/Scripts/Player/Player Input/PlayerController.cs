0683b4b0ca44f2539200bd5a220364456ab6d169
S1ren <sonictoby08@gmail.com>
2026-03-31 07:58:52 -0500

    Fixed inventory in pause menu bug

--- Cardomancers/Assets/Scripts/Player/Player Input/PlayerController.cs
+++ Cardomancers/Assets/Scripts/Player/Player Input/PlayerController.cs
@@ -103,16 +103,22 @@ public class PlayerController : MonoBehaviour
     public void OnEscape(InputAction.CallbackContext context)
     {
         pauseMenu.SetActive(!pauseMenu.activeInHierarchy);
+        if (Time.timeScale == 1f){
+                Time.timeScale = 0f;
+        } else
+            {
+              Time.timeScale = 1.0f;  
+            }
         switch(GameStateScript.CurrentState)
         {
             case GameStateScript.GameState.WALKING:
                 GameStateScript.CurrentState = GameStateScript.GameState.INVENTORY;
-                Time.timeScale = 0.0f;
                 break;
             case GameStateScript.GameState.INVENTORY:
                 GameStateScript.CurrentState = GameStateScript.GameState.WALKING;
-                Time.timeScale = 1.0f;
                 break;
+
+
 
         }
     }
@@ -129,6 +135,10 @@ public class PlayerController : MonoBehaviour
 
    public void OnToggleInventory(InputAction.CallbackContext context)
     {
+        if(pauseMenu.activeSelf)
+        {
+            return;
+        }
         //can only open the inventory when in free movement and alive
         if (GameStateScript.CurrentState == GameStateScript.GameState.WALKING && inventoryUIHandler.uiDisplayed == false)
         {
