7e7e6d0c2c5670da863778c758d4360744d69e0c
STUDENT\ad256400 <aaravdesai192010@gmail.com>
2026-04-07 09:22:02 -0500

    added in ability to change music from card shop to main theme

---
+++ Cardomancers/Assets/Scripts/ChangeMusic.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ChangeMusic : MonoBehaviour
+{
+    public void ChangeMusicMethod(AudioClip clip)
+    {
+        MusicPlayer musicPlayer = FindFirstObjectByType<MusicPlayer>();
+        if (musicPlayer == null) return;
+        if (!musicPlayer.Clips.Contains(clip)) musicPlayer.Clips.Add(clip);
+        musicPlayer.Pause();
+        
+        while (musicPlayer.AudioIndex != musicPlayer.Clips.IndexOf(clip))
+        {
+            musicPlayer.Next();
+        }
+        musicPlayer.Play();
+    }
+}
