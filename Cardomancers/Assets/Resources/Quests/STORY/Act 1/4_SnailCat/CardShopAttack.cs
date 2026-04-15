00d3c13512fc6fc077f5b400e976ea21f4145b3e
N-cataria <175165771+N-cataria@users.noreply.github.com>
2026-04-15 09:53:05 -0500

    commnet

--- Cardomancers/Assets/Resources/Quests/STORY/Act 1/4_SnailCat/CardShopAttack.cs
+++ Cardomancers/Assets/Resources/Quests/STORY/Act 1/4_SnailCat/CardShopAttack.cs
@@ -5,6 +5,7 @@ public class CardShopAttack : QuestStep
     private void Awake()
     {
         // make it so the card show door teleports the player to a different card shop where the reginald intimidation scene happens
+        // temporarily remove any battles the player might be able to enter into in the world
         BattleManager.instance.OnEnd.AddListener(this.FinishQuestStep);
     }
     
