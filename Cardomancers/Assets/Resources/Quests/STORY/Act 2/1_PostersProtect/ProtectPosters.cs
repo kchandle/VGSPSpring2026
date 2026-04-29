7843b8dd22ee4b92c70fab06fef7646d257bdfba
N-cataria <175165771+N-cataria@users.noreply.github.com>
2026-04-29 07:46:39 -0500

    Protect posters quest

--- Cardomancers/Assets/Resources/Quests/STORY/Act 2/1_PostersProtect/ProtectPosters.cs
+++ Cardomancers/Assets/Resources/Quests/STORY/Act 2/1_PostersProtect/ProtectPosters.cs
@@ -1,26 +1,55 @@
 using UnityEngine;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Random = UnityEngine.Random;
 
 public class ProtectPosters : QuestStep
 {
+    [Tooltip("A list containing the possible enemies who will be attacking the posters.")]
     [SerializeField] private GameObject[] enemies;
+    private GameObject[] posterLocations;
+    private List<GameObject> postersDefended;
+    private Dictionary<GameObject, PosterEnemy> enemiesInstantiated = new Dictionary<GameObject, PosterEnemy>();
+
+    private void Awake()
+    {
+        posterLocations = GameObject.FindGameObjectsWithTag("PosterLocation");
+        
+    }
     
     private void Start()
     {
-        foreach (GameObject go in GameObject.FindGameObjectsWithTag("PosterLocation"))
+        foreach (GameObject go in posterLocations)
         {
-            Instantiate(enemies[Random.Range(0, enemies.Length)], go.transform.position, Quaternion.identity);
+            int i = Random.Range(0, enemies.Length);
+            GameObject enemy = Instantiate(enemies[i], go.transform.position, Quaternion.identity);
+            enemiesInstantiated.Add(go, enemy.GetComponent<PosterEnemy>());
+            if (postersDefended.Contains(go))
+            {
+                enemy.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void DefendPoster(GameObject poster)
+    {
+        postersDefended.Add(poster);
+        enemiesInstantiated[poster].InitiateInteraction();
+        SetQuestStepState(JsonUtility.ToJson(postersDefended));
+        if (posterLocations.ToList() == postersDefended)
+        {
+            this.FinishQuestStep();
         }
     }
     
     protected override void SetQuestStepState(string state)
     {
-        throw new System.NotImplementedException();
+        postersDefended = JsonUtility.FromJson<List<GameObject>>(state);
     }
 
     public override string GetQuestStepState()
     {
-        throw new System.NotImplementedException();
+        return $"Posters defended: {postersDefended.Count} / {posterLocations.Length}";
     }
 }
