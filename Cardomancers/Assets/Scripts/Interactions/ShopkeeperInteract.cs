cd3827c058b2b532fa526026745bf9a7397ee997
Vivi C <amanchowdhury08@gmail.com>
2026-05-05 09:38:09 -0500

    Finished Shop UI and Settings Scene.

--- Cardomancers/Assets/Scripts/Interactions/ShopkeeperInteract.cs
+++ Cardomancers/Assets/Scripts/Interactions/ShopkeeperInteract.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using static GameStateScript;
 
 public class ShopkeeperInteract : MonoBehaviour
 {
     Shop shop;
+    GameState oldState;
+    [SerializeField] private GameObject UI;
     private void Awake()
     {
+
         shop = FindFirstObjectByType<Shop>(FindObjectsInactive.Include);
     }
 
     public void OnInteract()
     {
-        if (shop.IsShopOpenUI) shop.CloseShop();
-        else shop.OpenShop();
+        if (shop.IsShopOpenUI){
+
+            shop.CloseShop();
+            UI.SetActive(true);
+            oldState = GameStateScript.CurrentState;
+            GameStateScript.CurrentState = GameState.SHOPPING;
+
+        } else{
+
+            shop.OpenShop();
+            GameStateScript.CurrentState = oldState;
+            UI.SetActive(false);
+
+        }
     }
 }
